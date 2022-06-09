005373662cd4ebb42166ffb554d13091e1877bc2
ForEars <1228456740@qq.com>
2022-06-09 20:27:44 +0800

    完善了头像与用户名以及退出登录操作

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+function getUserInfo() {
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    // headers: {
+    //   Authorization: localStorage.getItem('token'),
+    // },
+    success: (res) => {
+      console.log(res)
+      if (res.status !== 0) return layer.msg(res.message)
+      layer.msg(res.message)
+      renderAvatar(res.data)
+    },
+  })
+}
+const renderAvatar = (user) => {
+  const name = user.nickname || user.username
+  $('#welcome').html(`欢迎 ${name}`)
+  if (user.user_pic !== null) {
+    $('.layui-nav-img').attr('src', user.user_pic).show()
+    $('.text-avatar').hide()
+  } else {
+    $('.layui-nav-img').hide()
+    let first = name[0].toUpperCase()
+    $('.text-avatar').html(first)
+  }
+}
+$('#tuichu').click(function () {
+  layer.confirm('确定退出登录？', { icon: 3, title: '提示' }, function (index) {
+    // 清空本地存储里面的 token
+    localStorage.removeItem('token')
+    // 重新跳转到登录页面
+    location.href = '/login.html'
+  })
+})
+getUserInfo()
